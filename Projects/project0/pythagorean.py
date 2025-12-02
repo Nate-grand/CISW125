f3666800a57c5af83f6572bd0065b7c10f2cb343
Nate-grand <nathan.t.macbeth@gmail.com>
2025-12-01 21:38:26 -0700

    finished madlib

---
+++ Projects/project0/pythagorean.py
@@ -0,0 +1,11 @@
+# nathan macbeth
+# 25/11/25
+# project #0.1 pythogorean theoram
+
+a = int(input("enter side a of the triangle: "))
+b = int(input("enter side b of the triangle: "))
+
+
+c = (a ** 2 + b ** 2) ** 0.5
+print(f"the side c is equal to: {c:.2f} ")
+
