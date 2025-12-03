785dd82c9cdd6c879a29c15e17b8b81e401fb587
Nate-Grand <nathan.t.macbeth@gmail.com>
2025-12-03 10:56:48 -0700

    Finished Types lab

---
+++ Labs/Types lab.py
@@ -0,0 +1,33 @@
+# nathan macbeth
+# 3/12/25
+# Types lab
+
+a = int(5)
+b = float(5)
+
+# Yes you can use both to do math
+
+a = int(input("New variable: "))
+b = float(input("New variable: "))
+
+# strings are when charcters are enclosed in quotation marks to be defined or to be printed
+
+c = ("Fairy tail is greatest show of all time")
+print(c)
+
+c = "Fairy tail is the greatest show of all time"
+mysubstring = c[3:43]
+mysubstring = mysubstring[20]
+print(mysubstring)
+
+# A list can contain data like numbers items people places pretty much anything that can be listed
+
+mylist = ["Apples","Pears","Watermelons","Mangos"]
+mylist.append("Tangerines")
+print(mylist)
+mylist.remove("Apples")
+print(mylist)
+
+print(len(mylist))
+
+# The two main things that i learned that are new in this lab were the substrings and lists. All the different things to remove add and do different things to lists i think will be helpfull in the future. The substring was interesting in how you could pick out a specific charcator from a long string which could be usefull.
