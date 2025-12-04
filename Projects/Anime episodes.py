f3d1e36330aee458c881b57e8d025838c0c1f20c
Nate-Grand <nathan.t.macbeth@gmail.com>
2025-12-04 10:50:06 -0700

    Finished Anime episodes

---
+++ Projects/Anime episodes.py
@@ -0,0 +1,13 @@
+# nathan macbeth
+# 4/12/25
+# Types project
+
+Number_of_animes = int(input("How many animes have you watched?: "))
+Average_number_episodes = int(input("Whats the average number of episodes of the animes youve watched?: "))
+
+Number_of_episodes_watched = (Number_of_animes * Average_number_episodes)
+
+print(f"The number of episodes watched is {Number_of_episodes_watched} ")
+
+
+
