680e226f367057ace1f17a97b4d0d2ca08753103
Nate-grand <nathan.t.macbeth@gmail.com>
2025-12-05 19:35:04 -0700

    changed Debug-1

--- Projects/project1/Debug-1.py
+++ Projects/project1/Debug-1.py
@@ -5,11 +5,15 @@
 
 # This program should add three numbers together.
 
+# Removed the total equals inbetween the input statements.
 
 num1 = int(input("What's the first number? >"))
 
 num2 = int(input("What's the second number? >"))
 
 num3 = int(input("What's the third number? >"))
+
+# Changed total equals num3 to adding all the nums together.
+
 total = num1 + num2 + num3
 print(f"Total is: {total}")
