df4a9bfd956e903b6f3b39ea9a8d43d70f310128
Nate-Grand <nathan.t.macbeth@gmail.com>
2026-01-14 11:04:05 -0700

    did shit

--- In class work/fighter.py
+++ In class work/fighter.py
@@ -1,6 +1,6 @@
-
-
-
+# nathan macbeth
+# 14/1/26
+# Fighter Game
 
 import pygame as py
 from Ball2 import Ball
@@ -66,12 +66,55 @@ while running:
 
             for bullet in bullets[:]:
                 if bullet.x < 0 or bullet.x > WIDTH or bullet.y < 0 or bullet.y > HEIGHT:
-                    bullet.remove(bullet)
+                    bullets.remove(bullet)
 
 
         for bullet in bullets[:]:
             bullet.move()
             bullet.drawBullet(screen)
+
+        for enemy in enemyBalls:
+            enemy.fall()
+            if mainball.collideCheck(enemy.x, enemy.y, enemy.radius):
+                if(not mainball.is_stunned()):
+                    mainball.hitValue -= 1
+                    enemyBalls.remove(enemy)
+                    enemyBalls.append(Ball(random.randint(50, WIDTH -50), random.randint(-50, 0), random.randint(5,10),BLUE,random.randint(6,7),'down',3))
+                    if mainball.hitValue < 1:
+                        print("Game Over")
+                        gameover = True
+                mainball.stun(1)
+
+            enemy.draw(screen)
+            for bullet in bullets[:]:
+                if bullet.collidecheck(enemy.x, enemy.y, enemy.radius):
+                    bullets.remove(bullet)
+                    enemy.hitvalue -= 1
+                    if enemy.hitvalue < 1:
+                        enemyBalls.remove(enemy)
+                        enemyBalls.append(Ball(random.randint(50, WIDTH -50), random.randint(-50, 0), random.randint(5, 10),(150,0,255),4,"down",7))
+
+            if enemy.y > HEIGHT:
+                enemyBalls.remove(enemy)
+                enemyBalls.append(Ball(random.randint(50, WIDTH -50), random.randint(-50, 0), random.randint(5,10),(150,150,250),"down",7))
+
+        mainball.move(keys, WIDTH, HEIGHT, False)
+        mainball.draw(screen)
+
+    elif gameover:
+        py.font.init()
+        font = py.font.SysFont('Arial', 72)
+        text = "GAME OVER!!!"
+        text1 = font.render(text, True, RED)
+        textwidth, textheight = font.size(text)
+
+        screen.blit(text1, ((WIDTH - textwidth) // 2),(HEIGHT - textheight) // 2)
+
+
+    py.display.update()
+    clock.tick(FPS)
+
+py.quit()
 
 
        
