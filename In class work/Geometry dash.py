df327b345a8acb01ee564a4a966a7ed362774f34
Nate-Grand <nathan.t.macbeth@gmail.com>
2026-02-05 10:59:41 -0700

    hghguyghugv

---
+++ In class work/Geometry dash.py
@@ -0,0 +1,17 @@
+import pygame as py
+
+py.init()
+
+size = WIDTH, HEIGHT = 560, 320
+
+screen = py.display.set_mode((size))
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 255)
+GRENY = (34, 84, 54)
+RED = (255,0, 0)
+
+FPS = 60
+clock = py.time.Clock()
