d692d0b1360ce62ad46b6e5a8d5043f06cedbf1e
Nate-Grand <nathan.t.macbeth@gmail.com>
2026-03-03 10:57:09 -0700

    yuyujguih

--- Java/FirstProgram.java
+++ Java/FirstProgram.java
@@ -1,6 +1,6 @@
-public class Main {
+public class FirstProgram {
 
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
 
         System.out.println("ydbuysbfuydbs");
 
