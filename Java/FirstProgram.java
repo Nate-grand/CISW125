c79a6228d53db84a02cac14f6eebf253660290ae
Nate-Grand <nathan.t.macbeth@gmail.com>
2026-03-04 10:59:11 -0700

    fytguyghu

--- Java/FirstProgram.java
+++ Java/FirstProgram.java
@@ -4,7 +4,7 @@ public class FirstProgram {
     public static void main(String[] args) {
         // This is pythagorean theroum.
         Scanner input = new Scanner(System.in);
-        System.out.println("This is the pythagorean theorum \n Please enter");
+        System.out.println("This is the pythagorean theorum \n Please enter A: ");
         int a = input.nextInt();
         System.out.println("b= ");
         int b = input.nextInt();
@@ -12,6 +12,13 @@ public class FirstProgram {
         double c = Math.sqrt(Math.pow(a,2) + Math.pow(b,2));
 
         System.out.print("The Hypotnuse is " + c);
+        input.nextLine();
+        System.out.println("What is your name? : ");
+        input.nextLine();
+        String name = input.nextLine();
+        System.out.println("welcome " + name);
+
+
 
         input.close();
          System.out.print(Integer.MAX_VALUE);
